304370f7c9e595cf602037d3be586f7d16fb818a
Sudeep Das <sudeepphys@gmail.com>
2013-04-22 02:35:13 -0500

    Working version
    State of code before interview

--- app/app.py
+++ app/app.py
@@ -1,64 +1,136 @@
-from flask import Flask, render_template, request
-#from rapid import (top_articles, search_articles, insert_article)
-from recommendationEngine import random_movie
+from flask import Flask, render_template, request, redirect,url_for,session, g, make_response
+import uuid
+
+from recommendationEngine import *
 app = Flask(__name__, static_folder="../static", static_url_path="/static")
 
 @app.route('/' ,methods=["GET", "POST"])
 def index():
-    #articles = top_articles()
-    movie = random_movie()
-    rats = {}
-    if request.method == "POST":
-        return do_submit(movie, rats)
-    else:
-        return render_template('myindex.jinja2.html',
-    page_submit="active", movie=movie)
+    #g.i = 0 
 
-    #return render_template('myindex.jinja2.html', rows = movies)
+    if request.method == "POST": 
+
+        print request.form
+        if request.form['uname'] != '':
+            uname = request.form['uname']
+            session['uname'] = request.form['uname']
+            session['uid'] = request.form['uname'] +'_'+ uuid.uuid1().hex
+            
+            return redirect(url_for('submit'))
+
+        else:
+           
+            return render_template('theindex.jinja2.html',page_submit="active")
+    else:   
+        
+        return render_template('theindex.jinja2.html',page_submit="active")
+
+@app.route('/submit/' ,methods=["POST","GET"])
+def submit():
+    #print "list", g.movies
+    #g.i += 1 
+    #print "iii", i 
+    print session['uid']
+    uname = session['uname']
+    print uname
+    
+    
+
+    #movie = random_movie()  
+    #movs += [movie]
+    #return render_template('myindex.jinja2.html',page_submit="active", movie=movie, uname=uname)
+    if request.method == "POST":
+        #movie = random_movie()             
+        return do_submit(uname)
+        
+    else:
+        #movie = random_movie()
+        return render_template('myindex.jinja2.html',page_submit="active", uname=uname)
+    
 
 @app.route('/about/')
 def about():
     return render_template('about.jinja2.html')
 
+# @app.after_request
+# def get_movie(response):
+#     movie = random_movie() 
+#     print "gg", movie
+#     return response
 
-def do_submit(movie, rats):
-    print "movie", movie
+
+@app.before_request
+def get_movie():
+    g.movie = random_movie()
+    #g.old = g.movie
+
+#     #print "gg", movie
+#     #return response
+
+# @app.after_request
+# def get_movie_again(response):
+#     g.movie = g.old
+#     print g.old
+#     return(response)
+
+def do_submit(uname):
+  
     form = request.form
     print "len", len(form.values()[0])
-    rating = 0 
-    #if form.values() = 
-    if len(form.values()[0]) ==0: 
-        return render_template('myindex.jinja2.html', page_submit="active",movie=movie)      
-    else:
-        rating = eval(form.values()[0])
-        print rating
-        
-        rats[movie['title']] = rating
-        #submission = form['rating']
-        #print submission
-        print rats
-        return render_template('myindex.jinja2.html', page_submit="active",movie=movie)   
+    rating = {}
+    score = 0 
+    print "form values", form.values()
     
-# @app.route('/search/<query>')
-# def search(query):
-#     articles = search_articles(query)
-#     return render_template('index.jinja2.html', 
-#                            query=query, 
-#                            articles=articles)
+    if len(form.values()[0]) == 0: 
+        
+        return render_template('myindex.jinja2.html', page_submit="active", uname=uname)      
+    else:
+        
+        score = eval(form.values()[0])
+        print score
+        rating['uid'] = session['uid']
+        rating['title'] = g.movie['title']
+        rating['score'] = score
+        
+        print rating 
+        
+        #print "ggg", g.movie
+        #mongo db call
+        nrecords = insert_rating(rating)
+        print "You have rated %d movies"%nrecords
+       
+        if (nrecords < 20):
+            return render_template('myindex.jinja2.html', page_submit="active", uname=uname)
+        else:
+            return render_template('myindex_reco.jinja2.html',page_submit="active", uname=uname) 
 
-# @app.route('/submit/', methods=["GET", "POST"])
-# def submit():
-#     if request.method == "POST":
-#         return do_submit()
-#     else:
-#         return render_template('submit.jinja2.html')
 
-# def do_submit():
-#     # article = Article(title=title, link=link)
-#     # insert_article(article)
-#     article = insert_article()
-#     return render_template("inserted.jinja2.html", article=article)
 
+
+
+@app.route('/reco/')
+def reco():
+    rows = {} 
+    uname = session['uname']  
+    rankings = get_recommendation(session['uid'])
+    print rankings
+    rows = []
+    for prediction, title in rankings:
+        row = {}
+        row['title'] = title
+        row['prediction']  = "%3.2f"%prediction
+        rows += [row]
+    print rows
+    return render_template('reco.jinja2.html', rows = rows, uname = uname)
+    
+@app.context_processor
+def utility_processor():
+  def get_random_movie():
+    g.movie = random_movie()['title']
+    return g.movie
+  return dict(get_random_movie=get_random_movie)
+
+app.secret_key = '\xaezX\xc7\xb2\x8d\xf7\x03~\xc1\xae\x0emn\x9aO\xc9$\xa8\x140\x0c\xdd'
 def run_devserver():
     app.run(debug=True)
 
--- app/recommendationEngine.py
+++ app/recommendationEngine.py
@@ -1,17 +1,71 @@
 import numpy
+from pymongo import MongoClient
+DBNAME = 'vistorRatings'
+SIMDBNAME = ''
+def get_collection():
+    return MongoClient()[DBNAME].ratings
+
+def random_movies():
+	curs = MongoClient()['MovieSimilarity_MLENS_1Million'].simTable
+	movies =  list(curs.find({},{'title':1,'_id':0}))
+	i = numpy.unique(numpy.random.randint(0,len(movies),size=200))
+	print i
+	
+	movs = [movies[k] for k in i]
+	print movs
+	return movs
+
+
 def random_movie():
-    movies = [
-        {"title": "Deer Hunter"},{"title":"GoodFellas"}
-        ]
-    i = numpy.int(numpy.random.rand()*60)
-    print i
+	curs = MongoClient()['MovieSimilarity_MLENS_1Million'].simTable
+	movies =  list(curs.find({},{'title':1,'_id':0}))
+	i = numpy.random.randint(0,len(movies))
+	print "in random", movies[i]
 
-    movies = movies * 30
+	return movies[i]
 
-    return movies[i]
 
-def search_articles(query):
-    return []
 
-def insert_article(article):
-    return False
+def insert_rating(rating):
+	coll = get_collection()
+	#coll
+	coll.insert(rating)
+	#Get this uid's count 
+	return len(list(coll.find({'uid':rating['uid']})))
+
+def get_recommendation(uid):
+	coll = get_collection()
+	cursor = MongoClient()['MovieSimilarity_MLENS_1Million'].simTable
+	a = coll.find({'uid':uid},{'title':1, '_id':0, 'score':1})
+	userRatings = list(a)
+	scores = {} 
+	totalSim = {}
+	hits = {}
+	for ur in userRatings:
+		print ur['title'], ur['score']
+		title = ur['title']
+		rating = ur['score']
+		matches = list(cursor.find({'title':title}))[0]
+		similar = simt = matches['similarTitles']
+
+		for (similarity, title2) in similar: 
+			#print similarity, title2
+			if title2 == title: continue 
+			scores.setdefault(title2,0) 
+			scores[title2] += similarity*rating 
+			totalSim.setdefault(title2,0) 
+			totalSim[title2] += similarity
+			hits.setdefault(title2,0) 
+			hits[title2] += 1
+	#print totalSim
+	for tit, hit in hits.items():
+		if hit < 5:
+			scores.pop(tit)
+			hits.pop(tit)
+			totalSim.pop(tit)
+	rankings = [(score/totalSim[item], item) for item, score in scores.items()]
+	rankings.sort()
+	rankings.reverse()
+	#print rankings
+	return rankings[:50]
+	
